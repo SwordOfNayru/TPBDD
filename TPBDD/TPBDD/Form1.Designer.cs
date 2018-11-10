3eb107584cd92efc4e84d97ae0eb64b700732a7c
SwordOfLink <theswordoflink@live.com>
2018-11-10 10:18:01 +0100

    Update TPBDD/TPBDD/Form1.Designer.cs

--- TPBDD/TPBDD/Form1.Designer.cs
+++ TPBDD/TPBDD/Form1.Designer.cs
@@ -34,11 +34,13 @@
             this.LBL_Pseudo = new System.Windows.Forms.Label();
             this.LBL_MotDePasse = new System.Windows.Forms.Label();
             this.B_Connect = new System.Windows.Forms.Button();
+            this.B_inscription = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.B_inscription);
             this.groupBox1.Controls.Add(this.B_Connect);
             this.groupBox1.Controls.Add(this.LBL_MotDePasse);
             this.groupBox1.Controls.Add(this.LBL_Pseudo);
@@ -46,7 +48,7 @@
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(302, 154);
+            this.groupBox1.Size = new System.Drawing.Size(302, 166);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -87,16 +89,25 @@
             // 
             this.B_Connect.Location = new System.Drawing.Point(9, 73);
             this.B_Connect.Name = "B_Connect";
-            this.B_Connect.Size = new System.Drawing.Size(287, 75);
+            this.B_Connect.Size = new System.Drawing.Size(287, 39);
             this.B_Connect.TabIndex = 4;
-            this.B_Connect.Text = "button1";
+            this.B_Connect.Text = "Connection";
             this.B_Connect.UseVisualStyleBackColor = true;
+            // 
+            // B_inscription
+            // 
+            this.B_inscription.Location = new System.Drawing.Point(9, 119);
+            this.B_inscription.Name = "B_inscription";
+            this.B_inscription.Size = new System.Drawing.Size(287, 41);
+            this.B_inscription.TabIndex = 5;
+            this.B_inscription.Text = "Inscription";
+            this.B_inscription.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 477);
+            this.ClientSize = new System.Drawing.Size(326, 192);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -114,6 +125,7 @@
         private System.Windows.Forms.Label LBL_Pseudo;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button B_inscription;
     }
 }
 
